30a2b140788c64fce021b6ee93a600cc63651244
Daweihao84 <dwsamurai84@gmail.com>
2024-05-09 23:58:37 +0800

    Add Color data.

---
+++ HW48-App store/AppStoreTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  AppStoreTableViewCell.swift
+//  HW48-App store
+//
+//  Created by Dawei Hao on 2024/5/9.
+//
+
+import UIKit
+
+class AppStoreTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ HW48-App store/Colors.swift
@@ -0,0 +1,18 @@
+//
+//  Colors..swift
+//  HW48-App store
+//
+//  Created by Dawei Hao on 2024/5/9.
+//
+
+import Foundation
+import UIKit
+
+struct Colors {
+    static let darkGray: UIColor  = UIColor.darkGray
+    static let lightGray: UIColor = UIColor.lightGray
+    static let black: UIColor     = UIColor.black
+    static let white: UIColor     = UIColor.white
+    static let blue: UIColor      = UIColor.systemBlue
+    static let clear: UIColor     = UIColor.clear
+}
