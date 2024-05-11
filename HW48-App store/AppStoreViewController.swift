d3b830011bc5b812a1b7799cbd0e718c06d0c885
Daweihao84 <dwsamurai84@gmail.com>
2024-05-11 17:37:27 +0800

    Import Kingfisher to scratch image.

--- HW48-App store/AppStoreViewController.swift
+++ HW48-App store/AppStoreViewController.swift
@@ -6,11 +6,15 @@
 //
 
 import UIKit
+import Kingfisher
 
 class AppStoreViewController: UIViewController {
-
+    
     let largetTitle: String = "Top Charts"
     
+    var appStoreData: AppStore?
+    
+    // MARK: - UI Setup:
     var segmenteControl = {
         let segmentedControl = UISegmentedControl()
         segmentedControl.insertSegment(withTitle: "Free App", at: 0, animated: true)
@@ -40,14 +44,21 @@ class AppStoreViewController: UIViewController {
         btn.configuration = config
         return btn
     } ()
- 
+    
+    var refreshControl: UIRefreshControl = {
+        let refreshControl: UIRefreshControl = UIRefreshControl()
+        refreshControl.tintColor = Colors.CustomTitleColor
+        return refreshControl
+    } ()
+    
     // MARK: - Life Cycle:
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        fetchData()
         setupUI()
-    }
         
+    }
+    
     // MARK: - Set up UI:
     func setupUI () {
         self.view.backgroundColor = Colors.CustomBackgroundColor
@@ -57,8 +68,10 @@ class AppStoreViewController: UIViewController {
         
         configFreeTableView()
         configPaidTableView()
+        
     }
-
+    
+    
     // MARK: - Set up NavigationView:
     func setNavigationView () {
         self.navigationController?.navigationBar.prefersLargeTitles = true
@@ -76,6 +89,7 @@ class AppStoreViewController: UIViewController {
         freeAppTableView.separatorStyle = .singleLine
         freeAppTableView.register(AppStoreTableViewCell.self, forCellReuseIdentifier: AppStoreTableViewCell.identifier)
         freeAppTableView.isScrollEnabled = true
+        freeAppTableView.refreshControl = refreshControl
     }
     
     func configPaidTableView () {
@@ -91,6 +105,7 @@ class AppStoreViewController: UIViewController {
     // MARK: - Add Targets:
     func addTargets () {
         segmenteControl.addTarget(self, action: #selector(segmentedControlValueChanged), for: .valueChanged)
+        refreshControl.addTarget(self, action: #selector(refreshControlActivited), for: .valueChanged)
     }
     
     // MARK: - Add Constraints:
@@ -140,35 +155,73 @@ class AppStoreViewController: UIViewController {
         }
     }
     
+    @objc func refreshControlActivited (_ sender: Any) {
+        refreshControl.endRefreshing()
+        print("DEBUG PRINT: refreshControl Activited ")
+    }
+    
+    // MARK: - Fetch Data:
+    func fetchData() {
+        let baseUrl: String = "https://rss.applemarketingtools.com/api/v2/tw/apps/top-free/25/apps.json"
+        guard let url = URL(string: baseUrl) else { return }
+
+        URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
+            
+            print(String(data: data!, encoding: .utf8) ?? "Invalid data")
+            
+            if let error = error {
+                print("Error fetching data: \(error.localizedDescription)")
+                return
+            }
+
+            guard let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) else {
+                print("Error with the response, unexpected status code: \(String(describing: response))")
+                return
+            }
+
+            guard let data = data else {
+                print("No data Received")
+                return
+            }
+            
+            let decoder = JSONDecoder()
+            do {
+                let appStoreDatas = try decoder.decode(AppStore.self, from: data)
+                DispatchQueue.main.async {
+                    self?.appStoreData = appStoreDatas
+                    self?.freeAppTableView.reloadData()
+                }
+            } catch {
+                print("Error decoding data: \(error.localizedDescription)")
+                print("Full error: \(error)")
+            }
+        }.resume()
+    }
 }
+
 
 // MARK: - Extension:
 extension AppStoreViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        switch tableView {
-            
-        case paidAppTableView:
-            print("DBUG PRINT: Change to paidTableView")
-            
-        case freeAppTableView:
-            print("DBUG PRINT: Change to freeAppTableView")
-            
-        default:
-            break
-        }
-        
-        return 1
+        print("\(appStoreData?.feed.results.count ?? 1)")
+        return appStoreData?.feed.results.count ?? 1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = freeAppTableView.dequeueReusableCell(withIdentifier: AppStoreTableViewCell.identifier, for: indexPath) as! AppStoreTableViewCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: AppStoreTableViewCell.identifier, for: indexPath) as! AppStoreTableViewCell
         
-        // data
+        let appStoreIndexPath = appStoreData?.feed.results[indexPath.row]
+        cell.appNameLabel.text       = appStoreIndexPath?.name
+        cell.numberLabel.text        = String(indexPath.row + 1)
+        cell.appDescripionLabel.text = appStoreIndexPath?.artistName
         
-        cell.appNameLabel.text       = "App Name"
-        cell.appDescripionLabel.text = "App Description"
-        cell.iconImageView.image     = UIImage(named: "01.png")
+        if let imageURL = appStoreIndexPath?.artworkUrl100, let url = URL(string: imageURL) {
+            cell.iconImageView.kf.setImage(with: url)
+            print("DEBUG PRINT: Kingfisher is working")
+        } else {
+            cell.iconImageView.image = UIImage(named: "01.png")
+            print("DEBUG PRINT: Kingfisher is not working.s")
+        }
         
         return cell
     }
