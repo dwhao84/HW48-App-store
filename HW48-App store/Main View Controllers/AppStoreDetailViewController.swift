000a3c4394f04f62b7db973866ed44607ad8fc84
Daweihao84 <dwsamurai84@gmail.com>
2024-05-16 23:44:11 +0800

    Add filter for indexPath.

--- HW48-App store/Main View Controllers/AppStoreDetailViewController.swift
+++ HW48-App store/Main View Controllers/AppStoreDetailViewController.swift
@@ -7,20 +7,27 @@
 
 import UIKit
 
-class AppStoreDetailViewController: UIViewController {
+class AppStoreDetailViewController: UIViewController, AppDetailTableViewCellDelegate {
+    
+    func appDetailTableViewCell(_ cell: AppDetailTableViewCell, didTapButton button: UIButton, buttonType: ButtonType) {
+        guard tableView.indexPath(for: cell) != nil else { return }
+        switch buttonType {
+        case .functionBtn:
+                    let content = "Share this App"
+                    let url = URL(string: "")
+            let activityVC = UIActivityViewController(activityItems: [content, url!], applicationActivities: nil)
+                    self.present(activityVC, animated: true)
+        case .shareBtn:
+            print(" ")
+        }
+    }
     
     let tableView: UITableView = {
         let tableView: UITableView = UITableView()
         tableView.translatesAutoresizingMaskIntoConstraints = false
         return tableView
     } ()
-    
-    let ratingTableView: UITableView = {
-        let tableView: UITableView = UITableView()
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-        return tableView
-    } ()
-    
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -37,13 +44,24 @@ class AppStoreDetailViewController: UIViewController {
         tableView.delegate   = self
         tableView.dataSource = self
         
-        tableView.register(AppDetailHeadView.nib(), forHeaderFooterViewReuseIdentifier: AppDetailHeadView.identifier)
+        // MARK: - HeaderView
+        tableView.register(AppDetailHeaderView.nib(), forHeaderFooterViewReuseIdentifier: AppDetailHeaderView.identifier)
         
+        // MARK: - TableViewCell
+        // MARK: AppDetailTableViewCell
         tableView.register(AppDetailTableViewCell.self, forCellReuseIdentifier: AppDetailTableViewCell.identifier)
         
-        tableView.rowHeight = 180
-        tableView.allowsSelection = false
+        // MARK: RatingTableViewCell
+        tableView.register(RatingTableViewCell.self, forCellReuseIdentifier: RatingTableViewCell.identifier)
+        
+        // MARK: WhatIsNewTableViewCell
+        tableView.register(WhatIsNewTableViewCell.self, forCellReuseIdentifier: WhatIsNewTableViewCell.identifier)
+        
+        // MARK: - tableView Setting:
+        tableView.separatorStyle = .singleLine
+        tableView.isScrollEnabled = true
     }
+
     
     func addConstraints () {
         view.addSubview(tableView)
@@ -57,42 +75,71 @@ class AppStoreDetailViewController: UIViewController {
 }
 
 extension AppStoreDetailViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    // MARK: TableViewCell:
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        switch tableView {
-        case tableView:
-            print("DEBUG PRINT: numberOfRowsInSection is 1")
-            return 1
-        default:
-            break
-        }
-        print("DEBUG PRINT: numberOfRowsInSection is 0")
-        return 0
+        return 4
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        switch tableView {
-        case tableView:
+       
+        // MARK: AppDetailTableViewCell
+        if indexPath.row == 0 {
+            
             let cell = tableView.dequeueReusableCell(withIdentifier: AppDetailTableViewCell.identifier, for: indexPath) as! AppDetailTableViewCell
+            cell.delegate = self
+            return cell
+        
+        // MARK: RatingTableViewCell
+        } else if indexPath.row == 1 {
+            
+            let cell = tableView.dequeueReusableCell(withIdentifier: RatingTableViewCell.identifier, for: indexPath) as! RatingTableViewCell
             return cell
             
+        // MARK: WhatIsNewTableViewCell
+        } else if indexPath.row == 2 {
             
-        default:
-            break
+            let cell = tableView.dequeueReusableCell(withIdentifier: WhatIsNewTableViewCell.identifier, for: indexPath) as! WhatIsNewTableViewCell
+            return cell
+            
+        } else if indexPath.row == 3 {
+            
         }
+        
         return UITableViewCell()
     }
     
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.row == 0 {
+            return 180
+        } else if indexPath.row == 1 {
+            return 100
+        } else if indexPath.row == 2{
+            return 150
+        }
+        return UITableView.automaticDimension
+    }
+    
+    
+    // MARK: - HeaderView:
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        let headView = AppDetailHeadView()
+        let headView = AppDetailHeaderView()
         return headView
     }
     
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        180
+        if section == 0 {
+            return 100
+        }
+        return 0
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        tableView.deselectRow(at: indexPath, animated: true)
+        print("DEBUG PRINT: deselectRow \(indexPath.row)")
+    }
 }
-
+    
 #Preview {
     UINavigationController(rootViewController: AppStoreDetailViewController())
 }
