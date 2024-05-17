d37ba1babfbeff06de15ffaf75625c620eb727bf
Daweihao84 <dwsamurai84@gmail.com>
2024-05-17 23:33:22 +0800

    Adjust the spacing.

--- HW48-App store/Constants/Colors.swift
+++ HW48-App store/Constants/Colors.swift
@@ -8,13 +8,13 @@
 import UIKit
 
 struct Colors {
-    static let darkGray: UIColor  = UIColor.darkGray
-    static let lightGray: UIColor = UIColor.lightGray
-    static let black: UIColor     = UIColor.black
-    static let white: UIColor     = UIColor.white
-    static let blue: UIColor      = UIColor.systemBlue
-    static let clear: UIColor     = UIColor.clear
-    static let pink: UIColor      = UIColor.systemPink
-    static let CustomTitleColor: UIColor = UIColor(named: "CustomColor") ?? UIColor.white
+    static let darkGray: UIColor              = UIColor.darkGray
+    static let lightGray: UIColor             = UIColor.lightGray
+    static let black: UIColor                 = UIColor.black
+    static let white: UIColor                 = UIColor.white
+    static let blue: UIColor                  = UIColor.systemBlue
+    static let clear: UIColor                 = UIColor.clear
+    static let pink: UIColor                  = UIColor.systemPink
+    static let CustomTitleColor: UIColor      = UIColor(named: "CustomColor") ?? UIColor.white
     static let CustomBackgroundColor: UIColor = UIColor(named: "CustomBackgroundColor") ?? Colors.black
 }
