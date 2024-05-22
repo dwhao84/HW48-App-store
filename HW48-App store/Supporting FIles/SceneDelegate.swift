63e30231caaa2b80bac2f007964b2091f26e4d9e
Daweihao84 <dwsamurai84@gmail.com>
2024-05-23 00:16:08 +0800

    Add UITabBarAppearance.

--- HW48-App store/Supporting FIles/SceneDelegate.swift
+++ HW48-App store/Supporting FIles/SceneDelegate.swift
@@ -28,8 +28,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             createArcadeNC(),
             createSearchNC()
         ]
-        window.rootViewController = tabBarController // Set the rootViewController of the window to the tabBarController.
-        tabBarController.selectedIndex = 2           // Set up index to specific VC.
+        
+        let appearance = UITabBarAppearance()
+        window.rootViewController = tabBarController  // Set the rootViewController of the window to the tabBarController.
+        tabBarController.selectedIndex = 2            // Set up index to specific VC.
+        tabBarController.tabBar.isTranslucent = true
+        tabBarController.tabBar.scrollEdgeAppearance = appearance
+        tabBarController.tabBar.standardAppearance   = appearance
         
         // Set the window and make it visible.
         self.window = window
